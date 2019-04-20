2ddffd808e2010fedd711fa4c1a9176c5866e070
NajamShehzad <najambutt195@gmail.com>
2019-04-21 00:07:26 +0500

    Classes just like Javascript class

---
+++ 51-61.py
@@ -0,0 +1,31 @@
+# How to use while loop python same as javscript
+num = 0
+while num < 10:
+    print("hello")
+    num += 1
+
+
+class Person:
+    def __init__(self, fullName):  # this is class constructor
+        self.name = fullName  # self is like "this" of javascript
+    def nameofPerson(self):
+        print(self.name)
+
+
+najam = Person("Najam Shehzad Butt")
+najam.nameofPerson()
+
+# It also pass referanc just like javascript
+newMember = najam
+print(newMember.name)
+najam.name = "Najam"
+print(newMember.name)
+a = 55
+b = a
+print(b)
+a = 70
+print(b)
+print(a)
+import sys
+numberVar = 5;
+print(sys.getsizeof(5))
