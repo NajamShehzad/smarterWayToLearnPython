edc8162131e7d18109941f516d17826bb2e03ea4
NajamShehzad <najambutt195@gmail.com>
2019-04-20 21:46:50 +0500

    Chapter 1-12 completed

---
+++ 1-11.py
@@ -0,0 +1,39 @@
+msg = "my name is najam"
+print(msg)
+
+# Numbers
+number = 12254
+number += 1
+print(number)
+newMeeage = "24" + "4"
+print(newMeeage)
+
+# Bodmas rules
+number11 = 8 * 8 / 8 + 25
+print(number11)
+
+# Concatenating strings
+firstName = "najam"
+lastName = "shehzad"
+fullName = firstName + lastName
+print(fullName)
+
+greeting = "Hello"
+separators = ", "
+addressee = "World"
+punc = "!"
+whole_greeting = greeting + separators + addressee + punc
+print(whole_greeting)
+
+# if statment
+if "name" == "name":
+    print("condition true")
+    print("if second line")
+print("testing spaces")
+
+if (5 > 5):
+    print("Najam")
+elif 6 == 6:
+    print("elif body")
+else:
+    print("else body")
